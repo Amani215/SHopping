23d81c52c00fffe9ab61d1d646732c467c94efcd
Amani215 <amanibrik5@gmail.com>
2021-11-13 14:41:20 +0100

    Changed: Splash exits with timer

--- app/src/main/java/com/example/shopping/SplashScreen.kt
+++ app/src/main/java/com/example/shopping/SplashScreen.kt
@@ -3,19 +3,17 @@ package com.example.shopping
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.view.WindowManager
+import java.util.*
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
+        Timer().schedule(object : TimerTask() {
+            override fun run() {
+                startActivity(Intent(applicationContext, MainActivity::class.java))
+            }
         }, 3000)
     }
 }
